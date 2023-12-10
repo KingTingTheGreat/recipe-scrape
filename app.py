5fcba44a1ee2cd2a471ce672ee185f05b3890c40
KingTingTheGreat <jting@bu.edu>
2023-12-10 16:26:15 -0500

    enable cors

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from recipe_scrapers import scrape_me
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.route('/api/scrape', methods=['GET', 'POST'])
+
+@app.route("/api/scrape", methods=["GET", "POST"])
 def scrape():
     response = {}
     try:
-        url:str = request.args.get('url')
+        url: str = request.args.get("url")
         scraper = scrape_me(url)
         response = scraper.to_json()
     except Exception as e:
-        print(f'Exception: {e}')
-        response['error'] = str(e)
+        print(f"Exception: {e}")
+        response["error"] = str(e)
     return jsonify(response)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
